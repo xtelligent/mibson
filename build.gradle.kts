746bb9211d11224634b915fa9e8035a52a2c35fd
xt-jorge <jorge@xtelligent.io>
2023-06-18 19:03:57 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.21"
+    id("maven-publish")
     application
 }
 
@@ -24,4 +25,21 @@ kotlin {
 
 application {
     mainClass.set("MainKt")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            /** Configure path of your package repository on Github
+             *  Replace GITHUB_USERID with your/organisation Github userID and REPOSITORY with the repository name on GitHub
+             */
+            url = uri("https://maven.pkg.github.com/xtelligent/mibson") // Github Package
+            credentials {
+                //Fetch these details from the properties file or from Environment variables
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
+            }
+        }
+    }
 }
